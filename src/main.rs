2139739c991b9e301a02988b3c04ed2484baf226
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-06-17 18:06:33 -0700

    hol up

--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,74 @@
 //!
 //! [actix]: https://actix.rs/docs/
 
-use actix_web::{server, App, HttpRequest, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::env;
+use std::sync::Mutex;
+use actix::prelude::*;
 
-fn greet(req: &HttpRequest) -> impl Responder {
-    let to = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", to)
+// An applications scope acts like a namespace for routes.
+// All routes for a specific scope, have the same url path prefix.
+// Application state is shared with all routes and resources *within the same scope*.
+
+// HttpServer accepts an application factory, as opposed to an instance,
+// constructing an instance for each thread.
+// You must use a shareable object if you want to share data betwixt threads.
+
+// web:Data uses Arc internally, thus to avoid double Arc, we should create our 
+// Data before registering it, using App::app_data()
+struct AppState {
+    // THIS MIGHT BE WHERE WE WANT TO USE OUR LOGIC GATES TO IMPLEMENT THE LS8.
+    app_name: String,
+
 }
 
-fn main() {
-    // Get the port number to listen on.
-    let port = env::var("PORT")
-        .unwrap_or_else(|_| "3000".to_string())
-        .parse()
-        .expect("PORT must be a number");
+struct MyActor {
+    count: usize,
+}
 
-    // Start a server, configuring the resources to serve.
-    server::new(|| {
+impl Actor for MyActor {
+    type Context = Context<Self>;
+}
+
+#[derive(Message)]
+#[rtype(result = "usize")]
+// This request handler, is an async function,
+// that accepts parameters, that can be extracted from a request,
+// for example, impl FromRequest, and returns a type that can
+// be cast to an HttpResponse, impl Responder.
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
+async fn index_2() -> impl Responder {
+    HttpResponse::Ok().body("Hello again!")
+}
+
+async fn index_3(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {}!", app_name)
+}
+
+
+// Next we have an instance of App from actix-web,
+// and we register the request handlers, with the applications,
+// route on a path, with a particular Http method.
+// This returns an Application factory.
+    //  Check out Factory Pattern
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let state = web::Data::new(AppState {
+        app_name: String::from("Rust2Webtris")
+    });
+    HttpServer::new(|| {
         App::new()
-            .resource("/", |r| r.f(greet))
-            .resource("/{name}", |r| r.f(greet))
+            .route("/rust2webtris", web::get().to(index_3))
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index_2))
     })
-    .bind(("0.0.0.0", port))
-    .expect("Can not bind to port 8000")
-    .run();
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
+        
 }
+
